d9ad0167fa85ccb3e7ae92a1373e3be867a91756
HauCG <maitrunghau9817189@gmail.com>
2024-10-24 16:10:18 +0700

    add

---
+++ src/main/java/org/example/demoservlet/ConverterServlet.java
@@ -0,0 +1,57 @@
+package org.example.demoservlet;
+//package org.example.demoservlet;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//@WebServlet(name = "LoginServlet", value = "/login")
+//public class LoginServlet extends HttpServlet {
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//        if ("hau".equals(username) && "hau".equals(password)) {
+//            writer.println("<h1>Welcome " + username + " to website</h1>");
+//        } else {
+//            writer.println("<h1>Login Error</h1>");
+//        }
+//        writer.println("</html>");
+//    }
+//}
+
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ConverterServlet", value = "/convert")
+public class ConverterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+    }
+}
